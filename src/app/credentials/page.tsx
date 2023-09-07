76eff755beaadffd61a8266fec229e83b8c3da56
AndreCouwberghs <andre.couwberghs@gmail.com>
2023-09-07 21:39:29 +0200

    #2 First simple usage of the seo possibilities of next.js

--- src/app/credentials/page.tsx
+++ src/app/credentials/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Credentials",
+};
+
 // `app/credentials/page.tsx` is the UI for the `/credentials` URL
 export default function Page() {
   return <h1>Credentials</h1>;
