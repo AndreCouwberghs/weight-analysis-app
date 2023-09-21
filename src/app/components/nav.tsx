80b8ab3f09828648cb4a72730bc8d5d7f2164b0f
AndreCouwberghs <andre.couwberghs@gmail.com>
2023-09-21 21:28:38 +0200

    #4 Use session in client component

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -2,8 +2,11 @@
 
 import Link from "next/link";
 import { usePathname } from 'next/navigation';
+import { useSession } from "next-auth/react";
+import { LoginButton, LogoutButton } from "./buttons.component";
 
 export default function Nav() {
+    const { data: session } = useSession();
     const pathname = usePathname();
 
     return <ul>
@@ -16,5 +19,16 @@ export default function Nav() {
         <li>
             <Link className={pathname === "/credentials" ? "active-menu-link" : ""} href="/credentials">Credentials</Link>
         </li>
+        {!session && (
+            <li>
+                <LoginButton />
+            </li>
+        )}
+        {session?.user && (
+            <li>
+                <LogoutButton />
+            </li>  
+        )}           
+        {session?.user?.name}
     </ul>;
 }
