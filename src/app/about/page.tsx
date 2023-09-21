cdde47326ac5c0ce801f26f3f9486ced0ce2474c
AndreCouwberghs <andre.couwberghs@gmail.com>
2023-09-21 21:29:20 +0200

    #4 Use session in server component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,23 @@
 import { Metadata } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 export const metadata: Metadata = {
   title: "About",
 };
 
 // `app/about/page.tsx` is the UI for the `/about` URL
-export default function Page() {
-  return <h1>About</h1>;
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  console.log(session);
+
+  return (
+    <>
+      <h1>About</h1>
+      {session?.user && (
+        <h3>[Testing] You are logged in as {session.user.name}</h3>
+      )}
+      {!session && <h3>[Testing] You are not logged in</h3>}
+    </>
+  );
 }
