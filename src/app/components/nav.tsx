00057711ecb026a679b26aef80760a5f736787d3
Nico Couwberghs <nico.couwberghs@gmail.com>
2023-09-28 16:02:11 +0200

    #1 Add basic hamburger menu

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -6,15 +6,24 @@ import { usePathname } from 'next/navigation';
 export default function Nav() {
     const pathname = usePathname();
 
-    return <ul className="nav">
-        <li>
-            <Link className={pathname === "/" ? "active-menu-link" : ""} href="/">Home</Link>
-        </li>
-        <li>
-            <Link className={pathname === "/about" ? "active-menu-link" : ""} href="/about">About</Link>
-        </li>
-        <li>
-            <Link className={pathname === "/credentials" ? "active-menu-link" : ""} href="/credentials">Credentials</Link>
-        </li>
-    </ul>;
+    return <section className="top-nav">
+        <div>
+            WAA
+        </div>
+        <input id="menu-toggle" type="checkbox" />
+        <label className='menu-button-container' htmlFor="menu-toggle">
+            <div className='menu-button'></div>
+        </label>
+        <ul className="menu">
+            <li>
+                <Link className={pathname === "/" ? "active-menu-link" : ""} href="/">Home</Link>
+            </li>
+            <li>
+                <Link className={pathname === "/about" ? "active-menu-link" : ""} href="/about">About</Link>
+            </li>
+            <li>
+                <Link className={pathname === "/credentials" ? "active-menu-link" : ""} href="/credentials">Credentials</Link>
+            </li>
+        </ul>
+    </section>;
 }
